a777f8037c45911f4a83e602006ca24235878f18
Era Dorta <e.dorta@tudelft.nl>
2024-11-28 09:50:31 +0100

    Fix podman flags for using GPUs

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -2,7 +2,9 @@ podman run -it \
        --name vitpose-dev \
        --rm \
        --shm-size=2gb \
-       --gpus all -e DISPLAY=:1 \
+       --device nvidia.com/gpu=all \
+       --security-opt=label=disable \
+       -e DISPLAY=:1 \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v $(pwd):/workspace \
        vitpose/vitpose:latest \
