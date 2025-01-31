46060b4de8f58bc4ea2cc1c39077cad2160e9abc
Era Dorta <e.dorta@tudelft.nl>
2025-01-31 13:14:11 +0100

    Add python entrypoint in slurm script

--- slurm/submit.sh
+++ slurm/submit.sh
@@ -14,6 +14,8 @@ apptainer run \
     --env PYTHONPATH=/workspace \
     --bind $(pwd):/workspace \
     --bind /tudelft.net/staff-umbrella/neon/experiments/VIT003/:/workspace/data/conflab \
-    /tudelft.net/staff-umbrella/neon/apptainer/vitpose-0.0.5.sif
+    /tudelft.net/staff-umbrella/neon/apptainer/vitpose-0.0.5.sif \
+    python /workspace/tools/train.py configs/ViTPose_coco_plus_conflab_w_bg_256x192.py \
+    --cfg-options model.pretrained=data/conflab/models/vitpose_base_coco_aic_mpii.pth --seed 0
 
 # sbatch --job-name vitpose-conflab --account ewi-insy-prb --partition insy,general submit.sh
